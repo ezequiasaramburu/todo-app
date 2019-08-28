2e5bbed9d3e00b36a3f42556c86690a00254b98b
Ezequias Aramburu <ezequias@MacBook-Pro-de-Ezequias.local>
2019-08-28 00:13:59 -0300

    add connection to mongo database

---
+++ backend/config/config.js
@@ -0,0 +1,13 @@
+const connection = {
+  uri: 'mongodb://127.0.0.1:27017/todos-db'
+};
+
+const serverPort = {
+  PORT: 4000
+};
+
+
+module.exports = { 
+  connection,
+  serverPort
+}
--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,20 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const PORT = 4000;
-
+const config = require('./config/config.js');
+const mongoose = require('mongoose');
+console.log(config);
 app.use(cors());
 
 app.use(bodyParser.json());
 
-app.listen(PORT, function() {
-    console.log('Server is running on Port: ' + PORT);
+mongoose.connect(config.connection.uri, { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+    console.log('MongoDB database connection established successfully');
+})
+
+app.listen(config.serverPort.PORT, function() {
+    console.log('Server is running on Port: ' + config.serverPort.PORT);
 });

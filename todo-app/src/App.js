36f4249813e999bce7504328a68441d9ad91e6d0
Ezequias Aramburu <ezequias@MacBook-Pro-de-Ezequias.local>
2019-08-30 01:15:36 -0300

    add router and basic components

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
+
+import TodosList from './components/TodosList';
+import CreateTodo from './components/CreateTodo';
 
 function App() {
   return (
-    <div className="container">
-      <h2>Todo App</h2>
-  </div>
+    <Router>
+      <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to="/" className="navbar-brand">Todo App</Link>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Todo</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <br/>
+          <Route path="/" exact component={TodosList} />
+          <Route path="/create" component={CreateTodo} />
+        </div>
+    </Router>
   );
 }
 

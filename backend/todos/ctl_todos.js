a46f5c5f9effab2124a3d24025b3abd93b5dc2ee
Ezequias Aramburu <ezequias@MacBook-Pro-de-Ezequias.local>
2019-09-03 01:17:34 -0300

    update CRUD methods

--- backend/todos/ctl_todos.js
+++ backend/todos/ctl_todos.js
@@ -3,56 +3,54 @@ const ObjectId = require('mongoose').Types.ObjectId;
 const { TASK_STATUS } = require('../const/const.js');
 
 module.exports.get = async function(req, res) {
-  console.log('===> GET');
   try {
     const todos = await TodoSchema.find({});
-    res.status(200).json({success: true, todos});
+    res.status(200).json({ success: true, todos });
 	} catch(error) {
     console.log(error);
-		res.status(400).json({success: false, msg: 'Somenthing went wrong fetching todos'});
+		res.status(400).json({ success: false, msg: 'Somenthing went wrong fetching todos' });
 	}
 };  
 
 module.exports.create = async function(req, res) {
-  console.log('===> Add', req.body);
   try {
     const { description } = req.body;
     const todo = await TodoSchema.create({ description });
-    res.status(200).json({success: true, msg: 'Todo added successfully', todo});
+    res.status(200).json({ success: true, msg: 'Todo added successfully', todo });
   } catch(error) {
     console.log(error);
-    res.status(400).json({success: false, msg: 'Somenthing went wrong adding new Todo'});
+    res.status(400).json({ success: false, msg: 'Somenthing went wrong adding new Todo' });
   }
 };
 
 module.exports.update = async function(req, res) {
-  console.log('===> Update', req.params);
-  try {
-    const { id }  = req.params;
-    const todoResolved = await TodoSchema.findOneAndUpdate(
-        { _id: ObjectId(id) },
-        { $set: {
-            status: TASK_STATUS.RESOLVED
+  const { id }  = req.params;
+  await TodoSchema.findByIdAndUpdate(
+      { _id: ObjectId(id) },
+      { $set: {
+          status: TASK_STATUS.RESOLVED
+        }
+      },
+      { new: true },
+      function(err, todoResolved) { 
+          if(err) {
+              console.log(error);
+              return res
+                      .status(400)
+                      .json({ success: false, msg: 'Somenthing went wrong marking Todo as Resolved' });
           }
-        });
-    console.log(todoResolved);
-    res.status(200).json({ success: true , todoResolved});
-  } catch(error) {
-    console.log(error);
-    res.status(400).json({ success: false, msg: 'Somenthing went wrong marking Todo as Resolved'});
-  }
+          return res.status(200).json({ success: true, todoResolved });
+      });
 };
 
-module.exports.delete = function(req, res) {
-  console.log('===> Delete');
+module.exports.delete = async function(req, res) {
   const { id } = req.params;
-  TodoSchema.findOneAndRemove({_id: id}, function(err, todo) {
+  await TodoSchema.findByIdAndRemove({ _id: id }, function(err, todo) {
     if(err) {
       console.log(error);
-      res.status(400).json({ success: false, msg: 'Somenthing went wrong deleting Todo'});
+      res.status(400).json({ success: false, msg: 'Somenthing went wrong deleting Todo' });
     }
-    console.log(todo);
-    res.send({ success: true });
+    return res.status(200).json({ success: true });
   });
 }
 

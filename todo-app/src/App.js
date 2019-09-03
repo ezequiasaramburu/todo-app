a9586c5538bfabb493731226f05680999ba4faeb
Ezequias Aramburu <ezequias@MacBook-Pro-de-Ezequias.local>
2019-09-03 19:02:06 -0300

    update todos url

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -10,21 +10,21 @@ function App() {
     <Router>
       <div className="container">
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link to="/" className="navbar-brand">Todo App</Link>
+            <Link to="/todos" className="navbar-brand">Todo App</Link>
             <div className="collpase navbar-collapse">
               <ul className="navbar-nav mr-auto">
                 <li className="navbar-item">
-                  <Link to="/" className="nav-link">Todos</Link>
+                  <Link to="/todos" className="nav-link">Todos</Link>
                 </li>
                 <li className="navbar-item">
-                  <Link to="/create" className="nav-link">Create Todo</Link>
+                  <Link to="/todos/create" className="nav-link">Create Todo</Link>
                 </li>
               </ul>
             </div>
           </nav>
           <br/>
-          <Route path="/" exact component={TodosList} />
-          <Route path="/create" component={CreateTodo} />
+          <Route path="/todos" exact component={TodosList} />
+          <Route path="/todos/create" component={CreateTodo} />
         </div>
     </Router>
   );
